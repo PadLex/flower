878a237f0596acdc026bb80aa68dc0605a02f132
Leo Kettmeir <10513.catzeir@gmail.com>
2018-10-25 15:01:17 +0200

    Clean Up

--- compiler.js
+++ compiler.js
@@ -1,114 +1,106 @@
 /*
 Compile code from standardised JSON.
- */
+*/
 
 const fs = require('fs');
 const standardise = require('./standardiser').standardise;
 
-module.exports.compile = function (source, destination){
-    console.log("compiling... hi2 %s", source);
+module.exports.compile = (source, destination) => {
+	console.log(`compiling... hi2 ${source}`);
 
-    if (source === destination){
-        console.error('Error: source is equal to destination');
-        return false
-    }
-    
-    const content = fs.readFileSync(source);
-    
-    standardise(content, (err, standardised) => {
-        if (err){
-            console.error(err);
-            return false
-        }
-    
-        console.log('\n' + JSON.stringify(standardised, null, 4));
-        
-        let script = '';
-        
-        
-        for (let p in standardised){
-            let parent = standardised[p];
+	if (source === destination) {
+		console.error('Error: source is equal to destination');
+		return false;
+	}
 
-            //Sort children to assign appropriate connections
-            // (redundant if standardiser already sorts but still necessary both for future proofing
-            // and to follow the JSON standard of not guaranteeing priority order)
-            parent.children.sort( (a, b) => a.order - b.order);
+	const content = fs.readFileSync(source);
 
-            /*
-            Add parent function:
-                function{
-                    call first function;
-                    call connections;
-                }
-             */
+	standardise(content, (err, standardised) => {
+		if (err) {
+			console.error(err);
+			return false;
+		}
 
-            let code = '';
+		console.log(`\n${JSON.stringify(standardised, null, 4)}`);
 
-            let connections = [{condition: 'true', target: parent.children[0].id}].concat(parent.connections);
-
-            connections.forEach( connection => {
-                code += callMethod(connection.condition, connection.target);
-            });
-
-            script += newMethod(p, code);
+		let script = '';
 
 
-            /*
-            Add child functions:
-                function{
-                    call connections;
-                    call next function;
-                }
-             */
+		for (const p in standardised) {
+			const parent = standardised[p];
 
-            for (let sc in parent.children){
-                let c = parseInt(sc);
-                let child = parent.children[c];
-                
-                code = child.code;
-                
-                if(c < parent.children.length - 1){
-                    child.connections.push({condition: 'true', target: parent.children[c+1].id});
-                }
+			//Sort children to assign appropriate connections
+			// (redundant if standardiser already sorts but still necessary both for future proofing
+			// and to follow the JSON standard of not guaranteeing priority order)
+			parent.children.sort((a, b) => a.order - b.order);
 
-                for(let cn in child.connections){
-                    let connection = child.connections[cn];
-                    
-                    code += callMethod(connection.condition, connection.target);
-                }
-                
-                script += newMethod(child.id, code);
-            }
-            
-        }
-        
-        
-        fs.writeFileSync(destination, script, 'utf8');
-    });
-    
-    
-    
-    //console.log(container);
+			/*
+			Add parent function:
+			function{
+			call first function;
+			call connections;
+		}
+		*/
+
+			let code = '';
+
+			const connections = [{condition: 'true', target: parent.children[0].id}].concat(parent.connections);
+
+			connections.forEach( connection => {
+				code += callMethod(connection.condition, connection.target);
+			});
+
+			script += newMethod(p, code);
+
+
+			/*
+			Add child functions:
+			function{
+			call connections;
+			call next function;
+		}
+		*/
+
+			for (const sc in parent.children){
+				const c = parseInt(sc, 10);
+				const child = parent.children[c];
+
+				code = child.code;
+
+				if (c < parent.children.length - 1){
+					child.connections.push({
+						condition: 'true',
+						target: parent.children[c + 1].id
+					});
+				}
+
+				for (const cn in child.connections){
+					const connection = child.connections[cn];
+
+					code += callMethod(connection.condition, connection.target);
+				}
+
+				script += newMethod(child.id, code);
+			}
+		}
+
+
+		fs.writeFileSync(destination, script, 'utf8');
+	});
+
+
+
+//console.log(container);
 };
 
-function newMethod(name, code){
-    return '\nmodule.exports.' + name + ' = function (file) {\n'
-        + '    ' + code
-        + '\n};\n'
+function newMethod(name, code) {
+	return `\nmodule.exports.${name} = function (file) {\n    ${code}\n};\n`;
 }
 
-function callMethod(condition, target){
+function callMethod(condition, target) {
+	if (condition === 'true') {
+		return `\nmodule.exports.${target}(file);`;
+	}
 
-    if(condition === 'true'){
-        return (
-            '\nmodule.exports.' + target + '(file);'
-        )
-    }
-
-    return (
-        'if(' + condition + '){'
-        + 'module.exports.' + target + '(file);'
-        + '}'
-    )
-
+	return `if (${condition}) {module.exports.${target}(file);}`;
 }
--- standardiser.js
+++ standardiser.js
@@ -5,177 +5,193 @@ Convert Draw.io uncompressed XML to standardised JSON format.
 
 const parseXML = require('xml2js').parseString;
 const unescape = require('unescape');
-const util = require('util');
 
 //Redundant until support for multiple formats
-exports.standardise = function(content, callback){
-    drawDotIo(content, callback);
-};
+exports.standardise = (content, callback) => drawDotIo(content, callback);
 
-function drawDotIo(xml, callback){
-    let idToName = {};
+function drawDotIo(xml, callback) {
+	const idToName = {};
 
-    parseXML(xml, (err, json) => {
-        if (err) {
-            callback(err, json);
-            return;
-        }
-        
-        const objects = json.mxGraphModel.root[0].mxCell;
-        
-        //Connections
-        let connections = {};
-        //Code
-        let children = {};
-        //Titles, no code
-        let parents = {};
-        
-        //Standardised JSON output, ready for compiling
-        let standardised = {};
-        
-        //Classify objects
-        for (var i in objects) {
-            var object = objects[i].$;
-            
-            if (!object.style) {
-                continue;
-            }
-            
-            if (object.source && object.target) {
-                connections[object.id] = {source: object.source, target: object.target};
-                continue;
-            }
-            
-            if (object.style.includes('shape=cloud')) {
-                parents[object.id] = {value: object.value, connections: [], start: true};
-                continue;
-            }
-            
-            if (object.parent == 1) {
-                parents[object.id] = {value: object.value, connections: [], children: [], start: false};
-                continue;
-            }
+	parseXML(xml, (err, json) => {
+		if (err) return callback(err, json);
 
-            console.log(objects[i].mxGeometry[0].$);
-            
-            if (object.parent) {
-                children[object.id] = {value: object.value, parent: object.parent, connections: [], y: objects[i].mxGeometry[0].$.y};
-            }
-            
-        }
-        
-        let id;
+		const objects = json.mxGraphModel.root[0].mxCell;
 
-        //Pass value to parent connections and remove "fake" children
-        for (id in children) {
-            var child = children[id];
-            if (child.parent in connections) {
-                connections[child.parent].value = child.value;
-                delete children[id];
-            }
-        }
+		//Connections
+		const connections = {};
+		//Code
+		const children = {};
+		//Titles, no code
+		const parents = {};
 
-        //Pass connections to real children
-        for (id in connections){
-            let connection = connections[id];
-            
-            let condition = connection.value;
+		//Standardised JSON output, ready for compiling
+		const standardised = {};
 
-            console.log(connection.target);
+		//Classify objects
+		for (const i in objects) {
+			const object = objects[i].$;
 
-            if(condition){
-                console.log(condition + ' - ' + condition.replace(/<(.|\n)*?>/g, '') + ' - ' + unescape(condition.replace(/<(.|\n)*?>/g, '')));
-                condition = unescape(condition.replace(/<(.|\n)*?>/g, ''));
-            }else{
-                condition = 'true'
-            }
-            
-            if (connection.source in parents){
-                parents[connection.source].connections.push({condition: condition, target: connection.target});
-            }
-            
-            if(connection.source in children){
-                children[connection.source].connections.push({condition: condition, target: connection.target});
-            }
-        }
+			if (!object.style) continue;
 
-        //Pass children to parents
-        for (id in children) {
-            var child = children[id];
+			if (object.source && object.target) {
+				connections[object.id] = {
+					source: object.source,
+					target: object.target
+				};
+				continue;
+			}
 
-            let code = child.value;
-            
-            if (code){
-                code = unescape(code.replace(/<(.|\n)*?>/g, ''));
-            }else{
-                code = ';';
-            }
+			if (object.style.includes('shape=cloud')) {
+				parents[object.id] = {
+					value: object.value,
+					connections: [],
+					start: true
+				};
 
-            console.log("Y: " + child.y);
-            
-            if (child.parent in parents){
-                parents[child.parent].children.push({id: id, code: code, connections: child.connections, order: child.y});
-            }
-            
-            if (child.parent in children){
-                callback("Error, nested children", json);
-                return false;
-            }
-            
-        }
+				continue;
+			}
 
-        // Sort children
-        // Make id's more human readable
-        // Add parents to output
-        for (id in parents) {
-            var parent = parents[id];
+			if (object.parent === "1") {
+				parents[object.id] = {
+					value: object.value,
+					connections: [],
+					children: [],
+					start: false
+				};
 
-            var title = parent.value;
+				continue;
+			}
 
-            idToName[id] = title;
+			console.log(objects[i].mxGeometry[0].$);
 
-            //Sort children to assign appropriate names
-            parent.children.sort( (a, b) => a.order - b.order);
+			if (object.parent) {
+				children[object.id] = {
+					value: object.value,
+					parent: object.parent,
+					connections: [],
+					y: objects[i].mxGeometry[0].$.y
+				};
+			}
+		}
 
-            let i = 0;
-            for (var childID in parent.children){
-                var child = parent.children[childID];
+		let id;
 
-                idToName[child.id] = title + '_' + i;
-                child.id = idToName[child.id];
+		//Pass value to parent connections and remove "fake" children
+		for (id in children) {
+			const child = children[id];
+			if (child.parent in connections) {
+				connections[child.parent].value = child.value;
+				delete children[id];
+			}
+		}
 
-                i++;
-            }
+		//Pass connections to real children
+		for (id in connections) {
+			const connection = connections[id];
+			let condition = connection.value;
 
-            if (parent.children.length > 0){
-                standardised[title] = {children: parent.children, connections: parent.connections};
-            }
+			console.log(connection.target);
 
-        }
+			if (condition) {
+				console.log(`${condition} - ${condition.replace(/<(.|\n)*?>/g, '')} - ${unescape(condition.replace(/<(.|\n)*?>/g, ''))}`);
+				condition = unescape(condition.replace(/<(.|\n)*?>/g, ''));
+			} else {
+				condition = 'true';
+			}
 
-        for (title in standardised) {
-            var parent = standardised[title];
+			if (connection.source in parents){
+				parents[connection.source].connections.push({
+					condition: condition,
+					target: connection.target
+				});
+			}
 
-            var renameConnections = [];
+			if (connection.source in children) {
+				children[connection.source].connections.push({
+					condition: condition,
+					target: connection.target
+				});
+			}
+		}
 
-            renameConnections = renameConnections.concat(parent.connections);
+		//Pass children to parents
+		for (id in children) {
+			const child = children[id];
+			let code = child.value;
 
-            for (var childID in parent.children) {
-                var child = parent.children[childID];
+			if (code) {
+				code = unescape(code.replace(/<(.|\n)*?>/g, ''));
+			} else {
+				code = ';';
+			}
 
-                renameConnections = renameConnections.concat(child.connections);
-            }
+			console.log(`Y: ${child.y}`);
 
-            renameConnections.forEach(connection => {
-                connection.target = idToName[connection.target];
-            });
+			if (child.parent in parents){
+				parents[child.parent].children.push({
+					id: id,
+					code: code,
+					connections: child.connections,
+					order: child.y
+				});
+			}
 
-        }
+			if (child.parent in children){
+				callback("Error, nested children", json);
+				return false;
+			}
+		}
 
-        callback(null, standardised);
+		// Sort children
+		// Make id's more human readable
+		// Add parents to output
+		for (id in parents) {
+			const parent = parents[id];
 
-    });
-    
-    //util.inspect(content);
-    
+			const title = parent.value;
+
+			idToName[id] = title;
+
+			//Sort children to assign appropriate names
+			parent.children.sort((a, b) => a.order - b.order);
+
+			let i = 0;
+			for (const childID in parent.children){
+				const child = parent.children[childID];
+
+				idToName[child.id] = `${title}_${i}`;
+				child.id = idToName[child.id];
+
+				i++;
+			}
+
+			if (parent.children.length > 0){
+				standardised[title] = {
+					children: parent.children,
+					connections: parent.connections
+				};
+			}
+		}
+
+		for (const title in standardised) {
+			const parent = standardised[title];
+			let renameConnections = [];
+
+			renameConnections = renameConnections.concat(parent.connections);
+
+			for (const childID in parent.children) {
+				const child = parent.children[childID];
+
+				renameConnections = renameConnections.concat(child.connections);
+			}
+
+			renameConnections.forEach(connection => {
+				connection.target = idToName[connection.target];
+			});
+		}
+
+		callback(null, standardised);
+	});
+
+	//util.inspect(content);
 }
